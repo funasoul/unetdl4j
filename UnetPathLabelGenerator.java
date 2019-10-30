2802a76861881efe4a23ee0b61c0d0218f7b4966
raguenets <montardon@users.noreply.github.com>
2019-10-30 11:54:49 +0100

    Initial commit
    Raw Java files.

---
+++ UnetPathLabelGenerator.java
@@ -0,0 +1,35 @@
+import org.datavec.api.io.labels.PathLabelGenerator;
+import org.datavec.api.writable.NDArrayWritable;
+import org.datavec.api.writable.Writable;
+import org.datavec.image.loader.NativeImageLoader;
+
+import java.io.IOException;
+import java.net.URI;
+
+public class UnetPathLabelGenerator implements PathLabelGenerator {
+    private NativeImageLoader imageLoader = new NativeImageLoader(256,256,1);
+    @Override
+    public Writable getLabelForPath(String path) {
+        try
+        {
+            String newPath = new String(path);
+            return new NDArrayWritable(imageLoader.asMatrix(newPath.replace("image","label")).divi(255.0f));
+        }
+        catch(IOException ioe)
+        {
+            ioe.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public Writable getLabelForPath(URI uri) {
+        return getLabelForPath(uri.getPath());
+    }
+
+    @Override
+    public boolean inferLabelClasses() {
+        return false;
+    }
+}
+
