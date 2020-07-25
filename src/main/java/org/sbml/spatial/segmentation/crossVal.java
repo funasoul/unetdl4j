acba238846fe92e1a1bddb37d07b873bcbb424e9
Medha-B <me1999bh@gmail.com>
2020-07-25 10:13:45 +0530

    Update CrossVal

--- src/main/java/org/sbml/spatial/segmentation/crossVal.java
+++ src/main/java/org/sbml/spatial/segmentation/crossVal.java
@@ -22,7 +22,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-
 public class crossVal {
 	
 	private static final Logger log = LoggerFactory.getLogger(crossVal.class);
