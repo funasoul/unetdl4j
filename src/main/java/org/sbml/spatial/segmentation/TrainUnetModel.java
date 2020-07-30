0688fafe7f0506f6c8790e5a9bd6ecec0218f450
Medha-B <me1999bh@gmail.com>
2020-07-31 03:04:36 +0530

    Restored to initial config: 117-125 (TrainUnetModel)

--- src/main/java/org/sbml/spatial/segmentation/TrainUnetModel.java
+++ src/main/java/org/sbml/spatial/segmentation/TrainUnetModel.java
@@ -11,7 +11,6 @@ import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
 import org.deeplearning4j.ui.api.UIServer;
 import org.deeplearning4j.ui.model.stats.StatsListener;
 import org.deeplearning4j.ui.model.storage.InMemoryStatsStorage;
-import org.deeplearning4j.zoo.ZooModel;
 import org.deeplearning4j.zoo.model.UNet;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
@@ -27,7 +26,9 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.HashMap;
 import java.util.Map;
-//import org.nd4j.linalg.learning.config.Adam;
+import org.nd4j.linalg.learning.config.Adam;
+//import org.deeplearning4j.zoo.ZooModel;
+
 
 public class TrainUnetModel {
 
@@ -82,7 +83,7 @@ public class TrainUnetModel {
       if (args.length > 0) {
         pathToImage = args[0];
       } else {
-    	  pathToImage = directory + File.separator + "dataset" + File.separator + "Raw images" + File.separator + "F01_621w1_crop13.tif";
+    	  pathToImage = directory + File.separator + "dataset" + File.separator + "raw_images" + File.separator + "F01_621w1_crop13.tif";
       }
 
       DataNormalization scaler = new ImagePreProcessingScaler(); // scale image between 0 and 1
@@ -114,14 +115,14 @@ public class TrainUnetModel {
 
       int numEpochs = 1;
 
-      //ComputationGraph model  = UNet.builder().updater(new Adam(1e-4)).build().init();
+      ComputationGraph model  = UNet.builder().updater(new Adam(1e-4)).build().init();
       //ComputationGraph model  = UNet.builder().updater(new Adam(new MapSchedule(ScheduleType.ITERATION, learningScheduleMap))).build().init();
       
       
       //To change the number of input channels from 3 to 1
-        ZooModel unet = UNet.builder().build();
-	    unet.setInputShape(new int[][]{{1, 128, 128}});
-	    ComputationGraph model = (ComputationGraph) unet.init();
+        //ZooModel unet = UNet.builder().build();
+	    //unet.setInputShape(new int[][]{{1, 128, 128}});
+	    //ComputationGraph model = (ComputationGraph) unet.init();
 	    
 	    StatsStorage ss = new InMemoryStatsStorage();
 	    uiServer.attach(ss);
