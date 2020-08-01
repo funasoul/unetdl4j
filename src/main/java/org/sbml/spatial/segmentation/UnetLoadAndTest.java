7d150de1a4ded469d791dea5dca7e8cc83867e55
Medha-B <me1999bh@gmail.com>
2020-08-01 23:22:45 +0530

    Updated

--- src/main/java/org/sbml/spatial/segmentation/UnetLoadAndTest.java
+++ src/main/java/org/sbml/spatial/segmentation/UnetLoadAndTest.java
@@ -29,7 +29,7 @@ public class UnetLoadAndTest {
 			log.info("*****LOAD MODEL******");
 			// Location where the model is saved
 			// File locationTosave = new File(home + File.separator + "unetSave.zip");
-			// Depends upon where the model weights are actually saved
+			//Depends upon where the model weights are actually saved
 			File locationTosave = new File(directory + File.separator + "unetSave.zip");
 			ComputationGraph model = ModelSerializer.restoreComputationGraph(locationTosave);
 
